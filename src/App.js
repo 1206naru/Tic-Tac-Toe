b77e0db4be3bc08c9e4f8d33b12bdeb5f446f61b
1206naru <159619027+1206naru@users.noreply.github.com>
2024-12-21 14:41:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css';
+import TicTac from './Components/TicTac/TicTac';
+const App = () => {
+  return (
+    <div>
+      <TicTac/>
+    </div>
+  )
+}
+
+export default App
